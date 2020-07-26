edc8173b10d0fb547ba29a1458e0d7d3548fb539
Saranya Chattopadhyay <55938071+SaranyaChattopadhyay@users.noreply.github.com>
2020-07-26 20:32:21 +0530

    Files uploaded

---
+++ task6.groovy
@@ -0,0 +1,63 @@
+job("Pull from GitHub ") {
+  description("Pulls Codes and scripts From GitHub repository and copies them into a folder in base system.")
+
+  scm {
+    github("SaranyaChattopadhyay/Jenkins-using-groovy", "master")
+  }
+
+  triggers {
+    scm(* * * * *)
+  }
+
+  steps {
+    shell('''
+    sudo mkdir /home/jenkins_groovy
+    sudo cp -rvf * /home/jenkins_groovy
+    ''')
+  }
+}
+
+job("Deploy page") {
+  description("Checks the language of code and deploys it on respective interpreter image.")
+
+  triggers {
+    upstream("Pull from GitHub")
+  }
+
+  steps {
+    shell(readFileFromWorkspace("deploy.sh"))
+  }
+}
+
+job("Test job") {
+  description("Tests whether the page has been deployed or not.")
+
+  triggers {
+    upstream("Deploy page")
+  }
+
+  steps {
+    shell(readFileFromWorkspace("test_app.sh"))
+  }
+}
+
+job("Mailing") {
+  description("If site is not working, it will mail the developer. ")
+
+  triggers {
+    upstream("Test job","FAILURE")
+  }
+
+  publishers {
+        mailer('chattopadhyaysaranya@gmail.com')
+  }
+}
+
+buildPipelineView("Deploy_groovy") {
+  title("Deploy webpage using Jenkins(auomated by groovy) and K8s")
+  selectedJob("1 Git Pull")
+  displayedBuilds(1)
+  refreshFrequency(4)
+  alwaysAllowManualTrigger()
+  showPipelineParameters()
+}
